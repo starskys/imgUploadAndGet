ee08d7476bcde9d1d44ae496ca24cb5a243b61b6
xuxiaoyin <1083516975@qq.com>
2017-07-02 23:28:34 +0800

    完善

--- TestController.java
+++ TestController.java
@@ -1,7 +1,7 @@
 
-package com.decent.game.manager.user.controller;
+package com.game.manager.user.controller;
 
-import com.decent.game.common.entity.MessageBean;
+import com.game.common.entity.MessageBean;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
